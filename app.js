5f4249f788b36b67137f19aec6ec4358a6453d06
Tom Boutell <tom@punkave.com>
2013-10-17 09:51:05 -0400

    Fancy sandbox, currently being used to test joins thoroughly

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var site = require('apostrophe-site')({
 
   // This line is required and allows apostrophe-site to use require() and manage our NPM modules for us.
   root: module,
-  shortName: 'apostrophe-sandbox',
-  hostName: 'apostrophe-sandbox',
-  title: 'Apostrophe Sandbox',
+  shortName: 'fancy-sandbox',
+  hostName: 'fancy-sandbox',
+  title: 'Fancy Sandbox',
   sessionSecret: 'apostrophe sandbox demo party',
   adminPassword: 'demo',
 
@@ -23,16 +23,119 @@ var site = require('apostrophe-site')({
       { name: 'marquee', label: 'Marquee' },
       { name: 'home', label: 'Home Page' },
       { name: 'blog', label: 'Blog' },
+      { name: 'events', label: 'Events' },
       { name: 'map', label: 'Map' },
-      { name: 'sections', label: 'Sections' }
+      { name: 'promoters', label: 'Promoters' },
+      { name: 'caterers', label: 'Caterers' },
+      { name: 'sections', label: 'Sections' },
+      { name: 'groups', label: 'Directory' }
     ]
   },
 
   // These are the modules we want to bring into the project.
   modules: {
     'apostrophe-blog':     { },
-    'apostrophe-map':      { },
-    'apostrophe-sections': { }
+    'apostrophe-events':     {
+      'addFields': [
+        // Join events with map locations
+        {
+          name: '_location',
+          type: 'joinByOne',
+          withType: 'mapLocation',
+          idField: 'locationId'
+        },
+        // ... And with promoters
+        {
+          name: '_promoters',
+          type: 'joinByArray',
+          withType: 'promoter',
+          idsField: 'promoterIds',
+          sortable: true
+        },
+        {
+          name: '_caterers',
+          type: 'joinByArray',
+          withType: 'caterer',
+          sortable: true,
+          idsField: 'catererIds',
+          relationshipsField: 'catererRelationships',
+          relationship: [
+            {
+              name: 'course',
+              label: 'Course',
+              type: 'select',
+              choices: [
+                {
+                  value: 'appetizer',
+                  label: 'Appetizer'
+                },
+                {
+                  value: 'entree',
+                  label: 'Entree'
+                },
+                {
+                  value: 'dessert',
+                  label: 'Dessert'
+                }
+              ]
+            },
+            {
+              name: 'dish',
+              label: 'Dish',
+              type: 'string'
+            },
+            {
+              name: 'donor',
+              label: 'Donor',
+              type: 'boolean'
+            }
+          ]
+        }
+      ]
+    },
+    'apostrophe-snippets':     { },
+    'apostrophe-map':      {
+      'addFields': [
+        {
+          name: '_events',
+          type: 'joinByOneReverse',
+          withType: 'event',
+          idField: 'locationId',
+          ifOnlyOne: true,
+          withJoins: [ '_promoters._events' ]
+        }
+      ]
+    },
+    'apostrophe-sections': { },
+    'apostrophe-people': { },
+    'apostrophe-groups': { },
+    'promoters': {
+      extend: 'apostrophe-snippets',
+      name: 'promoters',
+      instance: 'promoter',
+      addFields: [
+        {
+          name: '_events',
+          type: 'joinByArrayReverse',
+          withType: 'event',
+          idsField: 'promoterIds'
+        }
+      ]
+    },
+    'caterers': {
+      extend: 'apostrophe-snippets',
+      name: 'caterers',
+      instance: 'caterer',
+      addFields: [
+        {
+          name: '_events',
+          type: 'joinByArrayReverse',
+          withType: 'event',
+          idsField: 'catererIds',
+          relationshipsField: 'catererRelationships'
+        }
+      ]
+    }
   },
 
   // These are assets we want to push to the browser.
